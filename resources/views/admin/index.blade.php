a334880f4ff6ec54453851e4cf0e2523799ac805
SasoriAka96 <nahivallejo46@gmail.com>
2024-12-12 16:46:57 +0100

    cambios juevs

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -13,7 +13,7 @@ $userType = Auth::user()->userTypes;
 
 <div class="container-fluid">
   <div class="row">
-    <div class="sidebar border border-right col-md-3 col-lg-2 p-0 bg-body-tertiary">
+    <div class="sidebar border border-right col-md-30 col-lg-20 p-0 bg-body-tertiary">
       <div class="offcanvas-md offcanvas-end bg-body-tertiary" tabindex="-1" id="sidebarMenu" aria-labelledby="sidebarMenuLabel">
         <div class="offcanvas-header">
           <h5 class="offcanvas-title" id="sidebarMenuLabel">Panel Administrador</h5>
@@ -22,30 +22,30 @@ $userType = Auth::user()->userTypes;
 
         <!-- Datos del menu -->
          
-        <div class="offcanvas-body d-md-flex flex-column p-0 pt-lg-3 overflow-y-auto">
-          <ul class="nav flex-column">
+        <div class="offcanvas-body d-md-flex flex-row p-0 overflow-y-auto tw-w-full tw-justify-center">
+          <ul class="nav flex-row tw-justify-evenly tw-w-full">
             <li class="nav-item">
               <a class="nav-link d-flex align-items-center gap-2 active" aria-current="page" href="">
                 <p class="hover:tw-text-[#3cb4e5] tw-text-[#211261] m-0">Alumnos</p>
               </a>
             </li>
             <li class="nav-item">
-              <a class="nav-link d-flex align-items-center gap-2" href="#">
+              <a class="nav-link d-flex align-items-center gap-2" href="">
                 <p class="hover:tw-text-[#3cb4e5] tw-text-[#211261] m-0">Personal</p>
               </a>
             </li>
             <li class="nav-item">
-              <a class="nav-link d-flex align-items-center gap-2" href="#">
+              <a class="nav-link d-flex align-items-center gap-2" href="">
                 <p class="hover:tw-text-[#3cb4e5] tw-text-[#211261] m-0">Ciclos formativos</p>
               </a>
             </li>
             <li class="nav-item">
-              <a class="nav-link d-flex align-items-center gap-2" href="#">
+              <a class="nav-link d-flex align-items-center gap-2" href="">
                 <p class="hover:tw-text-[#3cb4e5] tw-text-[#211261] m-0">Modulos</p>
               </a>
             </li>
             <li class="nav-item">
-              <a class="nav-link d-flex align-items-center gap-2" href="#">
+              <a class="nav-link d-flex align-items-center gap-2" href="">
                 <p class="hover:tw-text-[#3cb4e5] tw-text-[#211261] m-0">Reuniones</p>
               </a>
           </ul>
@@ -53,5 +53,19 @@ $userType = Auth::user()->userTypes;
       </div>
     </div>
 </div>
+
+<!-- Dashboard -->
+<div>
+  <h1 class="tw-text-center tw-pt-10">DashBoard</h1>
+  <p>Numero de alumnos matriculados:</p>
+  <p>Numero de personal del instituto:</p>
+  <p>Numero de reuniones aceptadas para el dia de hoy:</p>
+  <p>Numero de reuniones pendientes para hoy:</p>
+  <p>Numero de reuniones totales a partir de hoy:</p>
+  <p>Numero de ciclos formativos:</p>
+  <p>Numero de usuarios sin rol:</p>
+  <p>Numero de modulos:</p>
+</div>
+  
     
 @endsection
