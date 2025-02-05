c7e3cc3345a26601ed8854a7b7482b9ee9039077
SasoriAka96 <nahivallejo46@gmail.com>
2025-02-05 15:33:22 +0100

    pantalla meetings avances

---
+++ resources/views/admin/meetings/showMeeting.blade.php
@@ -0,0 +1,25 @@
+@extends('layouts.app')
+@vite(['resources/css/app.css','resources/sass/app.scss', 'resources/js/app.js'])
+@section('content')
+
+
+    <div class = "tw-flex tw-flex-row tw-justify-evenly tw-w-full tw-pb-5">
+
+        <ul class="list-group tw-w-96">
+            <li class="list-group-item">Primer usuario:  {{ $meeting_user_user->firstUser->name}}</li>
+            <li class="list-group-item">Segundo usuario:  {{ $meeting_user_user->secondUser->name}}</li>
+            <li class="list-group-item">Estado:  {{ $meeting_user_user->status->code}}</li>
+            <li class="list-group-item">Dia:  {{ $meeting_user_user->day}}</li>
+
+        </ul>
+    </div>
+    <div class="tw-flex tw-flex-col tw-justify-center tw-w-full tw-pb-5 tw-items-center">
+        <a href="{{route('meetings.index')}}">
+            <button class="tw-bg-[#211261] tw-p-1 tw-text-white tw-rounded-lg tw-w-96" name="">
+                Volver
+            </button>
+        </a>
+    </div>
+
+
+@endsection
